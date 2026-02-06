b99e20c383947108ec6f75919c085017f7158f39
EmmanuelOseiMensah <accaoseimensah4christ32@gmail.com>
2026-02-06 22:39:05 +0000

    remove a dot to change background-color to blue: bg-gradient-to-br from-sky-400 to-blue-600

--- my-api-practice/src/App.jsx
+++ my-api-practice/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen .bg-gradient-to-br from-sky-400 to-blue-600 flex items-center justify-center px-4">
+    <div className="min-h-screen bg-gradient-to-br from-sky-400 to-blue-600 flex items-center justify-center px-4">
       <div className="w-full max-w-md bg-white rounded-2xl shadow-xl p-6">
         <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
           🌤 Weather App
